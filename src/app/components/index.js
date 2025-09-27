6318d9cd186ce9f5bfa0e91c36414859440640e9
2024103030073 <a.marcilei@estudante.ifro.edu.br>
2025-09-27 11:18:43 -0300

    teste

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,15 +1,28 @@
 'use client';
 import { useState } from 'react';
 import styles from './Header.module.css';
-
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <>
             <header className={styles.header}>
                 <h1 className={styles.Titulo}>
-                    Curiosidades e Tecnologia
+                    Curiosidades & Tecnologia
                 </h1>
+                <nav className={styles.navbar}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className={styles.li}>
+                            <Link href="/">Sobre</Link>
+                        </li>
+                        <li className={styles.li}>
+                            <Link href="/">Contato</Link>    
+                        </li>
+                    </ul>
+                </nav>
             </header>
         </>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,26 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
+      <section className={styles.section}>
+        <div className={styles.div01}>
+          <img src="/public/img/img01.png" alt="" />
+          <h2>Computadores pensam como Humanos</h2>
+          <p>Pesquisadores estão criando computadores neuromórficos, que tentam imitar o funcionamento do cérebro humano. Esses sistemas são mais rápidos e eficientes em tarefas de reconhecimento de padrões, como visão e fala, consumindo muito menos energia que os computadores tradicionais.</p>
+        </div>
+
+        <div className={styles.div02}>
+          <img src="/public/img/img01.png" alt="" />
+          <h2>O primeiro Email da História</h2>
+          <p>O primeiro e-mail foi enviado em 1971 por Ray Tomlinson, o inventor do “@”. Ele nem se lembra exatamente do conteúdo, mas diz que era algo como “QWERTYUIOP”. Hoje, bilhões de e-mails são enviados todos os dias, transformando a comunicação mundial.</p>
+        </div>
+
+        <div className={styles.div03}>
+          <img src="/public/img/img01.png" alt="" />
+          <h2>impresora 3D de comida</h2>
+          <p>Sim, é real! Impressoras 3D de comida conseguem criar pratos camada por camada, usando massas, chocolate e até carne cultivada em laboratório. A tecnologia promete revolucionar a indústria alimentícia, permitindo personalização e produção sustentável de alimentos.</p>
+        </div>
+
+      </section>
 
     </div>
   );
